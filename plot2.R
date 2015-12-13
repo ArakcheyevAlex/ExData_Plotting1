f35c0a33b8118940267808ba0ec67a485014eca9
Arakcheyev Alex <arakcheyev.alex@gmail.com>
2015-12-13 20:25:48 +0500

    Fix 2nd plot

--- plot2.R
+++ plot2.R
@@ -1,10 +1,11 @@
+library(sqldf)
+
 setwd("~/learning_r/ExData_Plotting1")
 fileUrl<- "../data/household_power_consumption.txt"
 
 powerConsumptionData <- read.csv.sql(fileUrl, sql = "SELECT * FROM file WHERE Date = '1/2/2007' OR Date = '2/2/2007'", header = TRUE, sep = ";")
-powerConsumptionData$Date <- as.Date(powerConsumptionData$Date, format = "%d/%m/%Y")
-powerConsumptionData <- data.frame(DateTime = strptime(paste(powerConsumptionData$Date, powerConsumptionData$Time), "%d/%m/%Y %H:%M:%S"), powerConsumptionData)
-
+powerConsumptionData <- data.frame(DateTime = paste(powerConsumptionData$Date, powerConsumptionData$Time), powerConsumptionData)
+powerConsumptionData$DateTime <- strptime(powerConsumptionData$DateTime, "%d/%m/%Y %H:%M:%S")
 Sys.setlocale("LC_TIME", "en_GB.UTF-8")
 
 png(file = "plot2.png", width = 480, height = 480)
